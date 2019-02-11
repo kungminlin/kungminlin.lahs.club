70d3828d0e928ba9465504ab71b9a9d11150655e
kungminlin <mml0816@gmail.com>
2019-02-10 23:40:23 -0800

    add horoscope

--- valentines-day-2019/assets/js/process.js
+++ valentines-day-2019/assets/js/process.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
       }
     })
     processLoveFortune($('#fortune-form').serializeArray())
+    $(this).closest('form').find("input:not(input[type='button'])").val("");
+    $(this).closest('form').find(".zodiac-img").text("");
   })
 
   // Test Form One
@@ -77,7 +79,7 @@ function processLoveFortune(d) {
     "<p><b>Your Personality:</b> " + data.zodiac.personality + "</p>" +
     "<p><b>Love Advice:</b> " + data.zodiac.horoscope + "</p>" +
     "<p><b>Most Compatible Signs:</b> " + data.zodiac.compatible + "</p>";
-  createModal({title: data['first-name'] + " " + data['last-name'], message: "Here is your fortune: test"});
+  createModal({title: data['first-name'] + " " + data['last-name'], message: forecast});
 }
 
 function processTestOne(d) {
@@ -143,7 +145,7 @@ var StarSigns = {
     compatible: "Scorpio and Pisces"
   },
   Leo: {name: "Leo", start_date: new Date("07 23"), end_date: new Date("08 22"),
-    personality: "You are like a Leo.",
+    personality: "Congratulations! You have found the first Easter Egg! Did you know? The creator of this website, Leo, is actually a Leo!",
     horoscope: "Love Leo. Be Leo.",
     compatible: "Leo"
   },
@@ -152,10 +154,34 @@ var StarSigns = {
     horoscope: "Your kind and supportive nature makes you a likeable and approachable person. Treat the person you love especially kindly today, and you may get something back from it. Don't let your boring, rational self show this Valentine's Day.",
     compatible: "Taurus and Capricorn"
   },
-  Libra: {name: "Libra", start_date: new Date("09 23"), end_date: new Date("10 22")},
-  Scorpio: {name: "Scorpio", start_date: new Date("10 23"), end_date: new Date("11 21")},
-  Sagittarius: {name: "Sagittarius", start_date: new Date("11 22"), end_date: new Date("12 21")},
-  Capricorn: {name: "Capricorn", start_date: new Date("12 22"), end_date: new Date("01 19")},
-  Aquarius: {name: "Aquarius", start_date: new Date("01 20"), end_date: new Date("02 18")},
-  Pisces: {name: "Pisces", start_date: new Date("02 19"), end_date: new Date("03 20")}
+  Libra: {name: "Libra", start_date: new Date("09 23"), end_date: new Date("10 22"),
+    personality: "You are very fixated on balance and harmony. You are obsessed with symmetry and strives to create equilibrium in all areas of life, including academics and romance. You highly value art, intellectualism, and connoisseurship. Because of this, you are naturally an excellent designer, decorator, art critic, and stylist.",
+    horoscope: "Your focus on balance has you hesitating in being more involved in a romantic relationship. Take this opportunity to take a different approach in life - try putting more of your time into romance. If you do so, your sense of balance and aesthetics will help you appeal to the one you love.",
+    compatible: "Leo and Sagittarius"
+  },
+  Scorpio: {name: "Scorpio", start_date: new Date("10 23"), end_date: new Date("11 21"),
+    personality: "You are a very emotional person on the inside, but you want to look strong to the ones around you. Nonetheless, your passion and emotional power is unrivaled. You may feel lonely, because you constantly crave physical closeness, spiritual illumination, and emotional intimacy.",
+    horoscope: "You are at the pinnacle of loneliness, and you do not wish to be more engaged in romantic affairs because of your apparent emotional strength and passion in a certain field. If you look around you closely this Valentine's Day, you will find a person who loves you more than anything. Passion and power are your most attractive points, afterall.",
+    compatible: "Leo, Scorpio, and Pisces"
+  },
+  Sagittarius: {name: "Sagittarius", start_date: new Date("11 22"), end_date: new Date("12 21"),
+    personality: "You are always on a quest for knowledge, constantly seeking geographical, intellectual, and spiritual adventures. You are a good storyteller and articulate your thoughts very well. Because of this, you can have a great sense of humor, but also a very sharp tongue.",
+    horoscope: "You are very interested in being in a romantic relationship, but your overt eagerness may instead alienate your potential chosen one. Take this time to calm down and see around you. Your intellect may find you a compatible mate, but your sharp tongue may chase them away.",
+    compatbile: "Leo, Aries, Aquarius, and Libra"
+  },
+  Capricorn: {name: "Capricorn", start_date: new Date("12 22"), end_date: new Date("01 19"),
+    personality: "You are skilled at navigating both the material and emotional realms. You are mischievous on the inside, but calm and keen on the outside. As you grow, you will become more and more youthful, optimistic, and playful.",
+    horoscope: "Your serious apperance may alienate your loved one, but you are afraid that your true mischievous self would do the same. It is time to combine your inner optimism and confidence with your acute, intellectual appeal this Valentine. If you can do so successfully, the one you love will surely come to you.",
+    compatible: "Pisces, Scorpio, Virgo, and Taurus"
+  },
+  Aquarius: {name: "Aquarius", start_date: new Date("01 20"), end_date: new Date("02 18"),
+    personality: "You are a scholar of humanities who express great sympathy for the weak. You aspire to change the world through social progress, and apply the same philosophy to your day-to-day life. You have an offbeat sense of fashion and eccentric hobbies.",
+    horoscope: "Your eccentricity is a double-edge sword, cutting away those who do not understand you and appealing to the interest of the curious ones. A sudden shift in your romantic relationship is expected, but if you try to conform instead of fight the change, maybe your romantic life will have a positive twist to it.",
+    compatible: "Gemini and Libra"
+  },
+  Pisces: {name: "Pisces", start_date: new Date("02 19"), end_date: new Date("03 20"),
+    personality: "You have absorbed every lesson - the joys and the pains and the hopes and the fears of life. Because of this, you are the most empathetic and compassionate out of all those around you. You get easily swallowed by emotions as a result, and often drift away from reality.",
+    horoscope: "Your radiance and compassion will find you a comparably compassionate and gentle partner. Do as the heart tells you, because you have a sharp intuition for emotions. However, do not get overwhelmed by emotions this Valentine's Day.",
+    compatible: "Scorpio and Cancer"
+  }
 }
